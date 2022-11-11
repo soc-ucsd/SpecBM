dddb97ef905aebf5fabc7546568daeb445d38b5b
FengYiLiao <86756536+FengYiLiao@users.noreply.github.com>
2022-11-10 17:36:31 -0800

    Add files via upload

---
+++ Experiement_2.m
@@ -0,0 +1,94 @@
+%First experiment: small-scale problem.
+%Goal: Show that the dual formulation is better when X^* is 'low' rank 
+
+% The penalty parameter is chosen by the fact that we already know the optimal solution 
+% Penal term is chosen as tr(X^*)*2+1 and tr(Z^*)*2+1
+
+%Author: Feng-Yi Liao
+
+clc;clear;
+addpath('.\packages\SBM-Primal');
+addpath('.\packages\SBM-Dual');
+addpath('.\packages\General');
+load('examples\n100m100dr97.mat');
+
+At_sdp        = full(At_sdp); 
+b_sdp         = full(b_sdp); 
+c_sdp         = full(c_sdp);
+opts.n        = K_sdp.s; 
+opts.m        = height(At_sdp); 
+opts.epislon  = 10^-20; 
+opts.beta     = 0.2; 
+opts.alpha    = 50; %does not matter for adaptive case 
+opts.feasible = false; 
+opts.adaptive = true;
+
+%%%%%%%%%% [Dual] %%%%%%%%%%
+
+    opts.Maxiter     = 200;
+    opts.rho         = Optimal.TrX*2+1;
+    opts.MaxCols     = 3; 
+    opts.EvecPast    = 2; 
+    opts.EvecCurrent = 1;
+    Out_Dual_2_1     = SBMD(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+
+    opts.Maxiter     = 200;
+    opts.rho         = Optimal.TrX*2+1;
+    opts.MaxCols     = 5; 
+    opts.EvecPast    = 4; 
+    opts.EvecCurrent = 1;
+    Out_Dual_4_1     = SBMD(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+
+    opts.Maxiter     = 200;
+    opts.rho         = Optimal.TrX*2+1;
+    opts.MaxCols     = 10; 
+    opts.EvecPast    = 9; 
+    opts.EvecCurrent = 1;
+    Out_Dual_9_1     = SBMD(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+  
+    opts.Maxiter     = 200;
+    opts.rho         = Optimal.TrX*2+1;
+    opts.MaxCols     = 3; 
+    opts.EvecPast    = 0; 
+    opts.EvecCurrent = 3;
+    Out_Dual_0_3     = SBMD(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+     
+    opts.Maxiter     = 200;
+    opts.rho         = Optimal.TrX*2+1;
+    opts.MaxCols     = 5; 
+    opts.EvecPast    = 0; 
+    opts.EvecCurrent = 5;
+    Out_Dual_0_5     = SBMD(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+    
+    opts.Maxiter     = 200;
+    opts.rho         = Optimal.TrX*2+1;
+    opts.MaxCols     = 10; 
+    opts.EvecPast    = 0; 
+    opts.EvecCurrent = 10;
+    Out_Dual_0_10    = SBMD(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+
+    
+%%%%%%%%%% [Primal] %%%%%%%%%%
+%We do not count the first iteration for SBMP
+    
+    opts.Maxiter     = 201;
+    opts.rho         = Optimal.TrZ*2+1;
+    opts.MaxCols     = 3;
+    opts.EvecPast    = 0;
+    opts.EvecCurrent = 3;
+    Out_Primal_0_3   = SBMP(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+   
+    opts.Maxiter     = 201;
+    opts.rho         = Optimal.TrZ*2+1;
+    opts.MaxCols     = 5;
+    opts.EvecPast    = 0;
+    opts.EvecCurrent = 5;
+    Out_Primal_0_5   = SBMP(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+    
+    opts.Maxiter     = 201;
+    opts.rho         = Optimal.TrZ*2+1;
+    opts.MaxCols     = 10;
+    opts.EvecPast    = 0;
+    opts.EvecCurrent = 10;
+    Out_Primal_0_10  = SBMP(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+    
---
+++ Plot_Experiment_1.m
@@ -0,0 +1,77 @@
+clc; clear; close;
+addpath('D:\Matlab\linspecer\');
+addpath('D:\Matlab\Professional Plots\STANDARDS\')
+PS = PLOT_STANDARDS();
+
+
+%parameter setting 
+width  = 8;     % Width in inches
+height = 6;    % Height in inches
+alw    = 0.75;    % AxesLineWidth
+fsz    = 11;      % Fontsize
+lw     = 1.5;      % LineWidth
+msz    = 8;       % MarkerSize
+
+filename_result = 'examples\Result\n100m100dr3\n100m100dr3-result-test6.mat';
+load(filename_result);
+filename_solution = 'examples\n100m100dr3';
+load(filename_solution);
+
+N = 9;%number of colors
+
+%range 
+xrange = 1:200;
+
+COLORS = [0.4660, 0.6740, 0.1880;
+          0.3010, 0.7450, 0.9330;
+          0.6350, 0.0780, 0.1840;
+          0, 0.4470, 0.7410;
+          0.8500, 0.3250, 0.0980;
+          0.4940, 0.1840, 0.5560;
+          1, 0, 0;
+          0, 1, 0;
+          0, 0, 1];
+
+
+figure();
+pos = get(gcf, 'Position');
+%set(gcf, 'Position', [pos(1) pos(2) width*100, height*100]); %<- Set size
+set(gcf, 'Position', [300 100  width*100, height*100]); %<- Set size
+set(gca, 'FontSize', fsz, 'LineWidth', alw); %<- Set properties
+
+semilogy(abs((-Out_Dual_0_3.Obj(xrange)-Optimal.Cost)/Optimal.Cost),"-.",'Color',COLORS(1,:),'LineWidth',lw);
+hold on 
+semilogy(abs((-Out_Dual_0_5.Obj(xrange)-Optimal.Cost)/Optimal.Cost),"-.",'Color',COLORS(2,:),'LineWidth',lw); 
+semilogy(abs((-Out_Dual_0_10.Obj(xrange)-Optimal.Cost)/Optimal.Cost),"-.",'Color',COLORS(3,:),'LineWidth',lw); 
+semilogy(abs((Out_Primal_2_1.Obj(xrange)-Optimal.Cost)/Optimal.Cost),"--",'Color',COLORS(4,:),'LineWidth',lw); 
+semilogy(abs((Out_Primal_4_1.Obj(xrange)-Optimal.Cost)/Optimal.Cost),"--",'Color',COLORS(5,:),'LineWidth',lw); 
+semilogy(abs((Out_Primal_9_1.Obj(xrange)-Optimal.Cost)/Optimal.Cost),"--",'Color',COLORS(6,:),'LineWidth',lw); 
+semilogy(abs((Out_Primal_0_3.Obj(xrange)-Optimal.Cost)/Optimal.Cost),"-",'Color',COLORS(7,:),'LineWidth',lw); 
+semilogy(abs((Out_Primal_0_5.Obj(xrange)-Optimal.Cost)/Optimal.Cost),"-",'Color',COLORS(8,:),'LineWidth',lw); 
+semilogy(abs((Out_Primal_0_10.Obj(xrange)-Optimal.Cost)/Optimal.Cost),"-",'Color',COLORS(9,:),'LineWidth',lw); 
+
+% semilogy(abs((-Out_Dual_0_3.Obj-Optimal.Cost)/Optimal.Cost),"-.",'LineWidth',lw);
+% hold on 
+% semilogy(abs((-Out_Dual_0_5.Obj-Optimal.Cost)/Optimal.Cost),"-.",'LineWidth',lw); 
+% semilogy(abs((-Out_Dual_0_10.Obj-Optimal.Cost)/Optimal.Cost),"-.",'LineWidth',lw); 
+% semilogy(abs((Out_Primal_2_1.Obj-Optimal.Cost)/Optimal.Cost),"--",'LineWidth',lw); 
+% semilogy(abs((Out_Primal_4_1.Obj-Optimal.Cost)/Optimal.Cost),"--",'LineWidth',lw); 
+% semilogy(abs((Out_Primal_9_1.Obj-Optimal.Cost)/Optimal.Cost),"--",'LineWidth',lw); 
+% semilogy(abs((Out_Primal_0_3.Obj-Optimal.Cost)/Optimal.Cost),"-",'LineWidth',lw); 
+% semilogy(abs((Out_Primal_0_5.Obj-Optimal.Cost)/Optimal.Cost),"-",'LineWidth',lw); 
+% semilogy(abs((Out_Primal_0_10.Obj-Optimal.Cost)/Optimal.Cost),"-",'LineWidth',lw); 
+
+
+legend('D(0,3)','D(0,5)','D(0,10)','P(2,1)','P(4,1)','P(9,1)',...
+       'P(0,3)','P(0,5)','P(0,10)','NumColumns',3);
+xlabel('Iteration','interpreter','latex');
+ylabel('$\frac{F-F^\star}{{|F^\star|}}$','interpreter','latex');
+title('$X_\star$ is high rank','interpreter','latex');
+set(gca,'FontSize',14);
+ax = gca;
+ax.YLabel.FontSize = 24;
+ylim([10^-9,10^4]);
+print("HighRank",'-depsc','-tiff');
+
+
+
