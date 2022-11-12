5c35c7f16a76cefab66c019593efe7b974e43664
FengYiLiao <86756536+FengYiLiao@users.noreply.github.com>
2022-11-12 08:58:01 -0800

    Alignment

--- packages/General/SedumiToMosek_Latest.m
+++ packages/General/SedumiToMosek_Latest.m
@@ -54,13 +54,13 @@ function prob = SedumiToMosek_Latest(At,b,c,K)
     [r, res] = mosekopt('symbcon echo(0)');
 
     %==========================================
-    LenFree = NumOfFree;
+    LenFree   = NumOfFree;
     LenNonneg = NumOfNonneg;
-    LenQuad = sum(K.q,'all');%total number of Quad variables
-    LenRQuad = sum(K.r,'all');%total number of Rotated Quad variables
+    LenQuad   = sum(K.q,'all');%total number of Quad variables
+    LenRQuad  = sum(K.r,'all');%total number of Rotated Quad variables
     LenLinear = NumOfFree+LenNonneg+LenQuad+LenRQuad;
-    steps = K.s.*(K.s+1)./2;
-    LenPSD = sum(steps,'all'); %total number of PSD variables (only symmetric part)   
+    steps     = K.s.*(K.s+1)./2;
+    LenPSD    = sum(steps,'all'); %total number of PSD variables (only symmetric part)   
     
     
     [hei,wei] = size(At);
@@ -68,50 +68,50 @@ function prob = SedumiToMosek_Latest(At,b,c,K)
     %At_Free = At(:,1:NumOfFree);
     %At_Quad = At(:,NumOfFree+1:NumOfFree+LenQuad-1);
     At_Linear = At(:,1:LenLinear);
-    At_PSD = At(:,LenLinear+1:end);
+    At_PSD    = At(:,LenLinear+1:end);
     
        
     %Second Order Cone
     if (LenQuad~=0)
-        QUADIDX = zeros(1,LenQuad);
-        QUADSUB = [1,K.q(1:end-1)]; %tricky step
-        QUADSUB = cumsum(QUADSUB);
+        QUADIDX   = zeros(1,LenQuad);
+        QUADSUB   = [1,K.q(1:end-1)]; %tricky step
+        QUADSUB   = cumsum(QUADSUB);
         QUASDCONE = repmat(res.symbcon.MSK_CT_QUAD,1,NumOfQuad);
-        start = 1;
-        Offset = NumOfFree+LenNonneg; %% notice the difference with Rotated Second constraint
+        start     = 1;
+        Offset    = NumOfFree+LenNonneg; %% notice the difference with Rotated Second constraint
         for i = 1:NumOfQuad
-            step = K.q(i);
+            step                        = K.q(i);
             QUADIDX(start:start+step-1) = (1:K.q(i))+Offset;
-            start = start + K.q(i);
-            Offset = Offset + step;
+            start                       = start + K.q(i);
+            Offset                      = Offset + step;
         end
-        prob.cones.sub = QUADIDX;
+        prob.cones.sub    = QUADIDX;
         prob.cones.subptr = QUADSUB;
-        prob.cones.type = QUASDCONE;
+        prob.cones.type   = QUASDCONE;
     end
     
     %Rotated Second Order Cone 
     if (LenRQuad ~= 0)
-        RQUADIDX = zeros(1,LenRQuad);
-        RQUADSUB = [1,K.r(1:end-1)]; %tricky step
-        RQUADSUB = cumsum(RQUADSUB);      
+        RQUADIDX   = zeros(1,LenRQuad);
+        RQUADSUB   = [1,K.r(1:end-1)]; %tricky step
+        RQUADSUB   = cumsum(RQUADSUB);      
         RQUASDCONE = repmat(res.symbcon.MSK_CT_RQUAD,1,NumOfRQuad);%%
-        start = 1;
-        Offset =  NumOfFree+LenNonneg+NumOfQuad; %% notice the difference with Rotated Second constraint
+        start      = 1;
+        Offset     = NumOfFree+LenNonneg+NumOfQuad; %% notice the difference with Rotated Second constraint
         for i = 1:NumOfRQuad
-            step = K.r(i);
+            step                         = K.r(i);
             RQUADIDX(start:start+step-1) = (1:K.r(i))+Offset;
-            start = start + K.r(i);
-            Offset = Offset + step;
+            start                        = start + K.r(i);
+            Offset                       = Offset + step;
         end
         if LenQuad ~=0
-            prob.cones.sub = [prob.cones.sub, RQUADIDX];
+            prob.cones.sub    = [prob.cones.sub, RQUADIDX];
             prob.cones.subptr = [prob.cones.subptr,RQUADSUB];
-            prob.cones.type = [prob.cones.type,RQUASDCONE];   
+            prob.cones.type   = [prob.cones.type,RQUASDCONE];   
         else
-            prob.cones.sub =  RQUADIDX;
+            prob.cones.sub    =  RQUADIDX;
             prob.cones.subptr = RQUADSUB;
-            prob.cones.type = RQUASDCONE;    
+            prob.cones.type   = RQUASDCONE;    
         end
     end
     
@@ -122,32 +122,33 @@ function prob = SedumiToMosek_Latest(At,b,c,K)
    
     %PSD Cone (Preparation)
     IntIdxsPSD = zeros(1,LenPSD);
+
     %fisrt construct the corresponding idx, row, column of each variables 
-    PSDIDX = zeros(1,LenPSD);
-    symrows = zeros(1,LenPSD);
-    symcols = zeros(1,LenPSD);
-    start = 1;
+    PSDIDX     = zeros(1,LenPSD);
+    symrows    = zeros(1,LenPSD);
+    symcols    = zeros(1,LenPSD);
+    start      = 1;
     OffSet = 0; %For InterestedIdx
     for i = 1:NumOfPSD
-        step = K.s(i)*(K.s(i)+1)/2;
-        PSDIDX(start:start+step-1) = i;
-        [IndSym,~] = SymmetricIndices(K.s(i));
-        [row,col]=ind2sub([K.s(i),K.s(i)],IndSym);
+        step                        = K.s(i)*(K.s(i)+1)/2;
+        PSDIDX(start:start+step-1)  = i;
+        [IndSym,~]                  = SymmetricIndices(K.s(i));
+        [row,col]                   = ind2sub([K.s(i),K.s(i)],IndSym);
         symrows(start:start+step-1) = row;
         symcols(start:start+step-1) = col;
         IntIdx = IndSym';
         if i>1
             OffSet = OffSet + K.s(i-1)^2;
         end
-        IntIdx = IntIdx+OffSet;
+        IntIdx                         = IntIdx+OffSet;
         IntIdxsPSD(start:start+step-1) = IntIdx;
-        start = start+step;
+        start                          = start+step;
     end
        
     
     %Objective
     c_lin = c(1:LenLinear); %free part of c
-    c_s = c(LenLinear+1:end); %PSD part of c
+    c_s   = c(LenLinear+1:end); %PSD part of c
     
     %Obj_linear part
     prob.c = c_lin;
@@ -162,11 +163,11 @@ function prob = SedumiToMosek_Latest(At,b,c,K)
     
     
     %Obj_PSD
-    [r,~,v]=find(c_s(IntIdxsPSD));
+    [r,~,v]        = find(c_s(IntIdxsPSD));
     prob.barc.subj = PSDIDX(r);
     prob.barc.subk = symrows(r);
     prob.barc.subl = symcols(r);
-    prob.barc.val = v'; 
+    prob.barc.val  = v'; 
 
 
     %Constraint
@@ -174,20 +175,20 @@ function prob = SedumiToMosek_Latest(At,b,c,K)
     %if NumOfFree ~= 0 || NumQuad ~=0
     if LenLinear ~=0
         %[r,c,v]=find(At(:,1:NumOfFree+LenQuad));
-        [r,c,v]=find(At_Linear);
-        prob.a = sparse(r,c,v,m,LenLinear);
+        [r,c,v] = find(At_Linear);
+        prob.a  = sparse(r,c,v,m,LenLinear);
     else
-        prob.a = sparse([], [], [], m, 0); 
+        prob.a  = sparse([], [], [], m, 0); 
     end    
     
         
     %constraint_psd part
-    [r,c,v] = find(At_PSD(:,IntIdxsPSD));
+    [r,c,v]        = find(At_PSD(:,IntIdxsPSD));
     prob.bara.subk = symrows(c);
     prob.bara.subl = symcols(c);
     prob.bara.subi = r';
     prob.bara.subj = PSDIDX(c);
-    prob.bara.val = v';    
+    prob.bara.val  = v';    
     
     
 end
