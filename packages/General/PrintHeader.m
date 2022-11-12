b74bf6a7623f5ddc36b23a8b0a167940edc926a6
Yang Zheng <zhengy093@gmail.com>
2022-11-11 20:34:18 -0800

    update output information

--- packages/General/PrintHeader.m
+++ packages/General/PrintHeader.m
@@ -4,12 +4,17 @@ function PrintHeader(proctime,opts)
     if opts.verbose
         % Set method to display
         %method = 'SBMP';
+        [header,myline1,myline2] = Header();
         method = opts.method;
 %         if strcmpi(opts.solver,'hsde')
 %             method = 'homogeneous self-dual embedding';
 %         else
 %             method = opts.solver;
 %         end
+        fprintf(myline1)
+        fprintf('Spectral Bundle Method for Primal SDPs - v1.0\n')
+        fprintf('Authors: F. Liao, Y. Zheng @SOC Lab \n')
+        fprintf(myline1)
         fprintf('Initilization is done in %.4f seconds.      \n',proctime);
         fprintf('Algorithm              : %s\n',method);
 
@@ -17,19 +22,18 @@ function PrintHeader(proctime,opts)
 %         fprintf('Adaptive penalty       : %i\n',opts.adaptive);
 %         end
 %         fprintf('Scale data             : %i\n',opts.rescale);
-%         fprintf('Free variables         : %i                \n',K.f);
-%         fprintf('Non-negative variables : %i                \n',K.l);
-%         fprintf('Second-order cones     : %i (max. size: %i)\n',length(find(K.q ~=0)),max(K.q));
-%         fprintf('Semidefinite cones     : %i (max. size: %i)\n',length(find(K.s ~=0)),max(K.s));
-%         fprintf('Affine constraints     : %i                \n',opts.m);
-%         if any(strcmpi(opts.solver,{'primal','dual','hsde'}))
-%         fprintf('Consensus constraints  : %i                \n',sum(accumarray(Ech,1)));
-%         else
-%         fprintf('Nonorthogonal dimension: %i                \n',opts.sos.NonOrth);
-%         end
-        [header,myline1,myline2] = Header();
-        fprintf(myline1);
-        fprintf(header);
-        fprintf(myline2);
+         fprintf('Free variables         : %i                \n',opts.K.f);
+         fprintf('Non-negative variables : %i                \n',opts.K.l);
+         fprintf('Second-order cones     : %i (max. size: %i)\n',length(find(opts.K.q ~=0)),max(opts.K.q));
+         fprintf('Semidefinite cones     : %i (max. size: %i)\n',length(find(opts.K.s ~=0)),max(opts.K.s));
+         fprintf('Affine constraints     : %i                \n',opts.m);
+
+         fprintf('Penalty parameter      : %.2f                \n',opts.rho);
+         fprintf('Current eigenvectors   : %i                  \n',opts.current);
+         fprintf('Past eigenvectors      : %i                \n',opts.past);
+
+         fprintf(myline2);
+         fprintf(header);
+         fprintf(myline2);
     end
 end
