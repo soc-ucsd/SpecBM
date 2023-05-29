0004295378609914f33f1eafba47661e1b2d75a1
FengYiLiao <86756536+FengYiLiao@users.noreply.github.com>
2023-05-29 10:41:54 -0700

    Delete packages directory

--- packages/General/Header.m
+++
@@ -1,5 +0,0 @@
-function [header,myline1,myline2] = Header
-% Set stuff
-myline1 = [repmat('=',1,78),'\n'];
-myline2 = [repmat('-',1,78),'\n'];
-header  = [' iter|   Obj   | RelAccry |  PFeasi  |  DFeasi  |   Gap   |    alpha  | time (s) | Comple\n'];
--- packages/packages/SBM-Primal/CheckSparsityPattern.m
+++
@@ -1,5 +0,0 @@
-function CheckSparsityPattern(Paras)
-    [row,col]                = find(Paras.At_sdp);
-    Paras.At_sdp_nonzero.row = row;
-    Paras.At_sdp_nonzero.col = col;
-end
