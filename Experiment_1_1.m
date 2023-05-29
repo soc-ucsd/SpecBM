fdcbf10bb88badbe8302d1110a6be21bd5c36782
FengYiLiao <86756536+FengYiLiao@users.noreply.github.com>
2023-05-29 10:46:20 -0700

    Add files via upload

--- Experiment_1_1.m
+++ Experiment_1_1.m
@@ -3,7 +3,7 @@
 %Instance: rank(X^*) = 97
 
 % The penalty parameter is chosen by the fact that we already know the optimal solution 
-% Penal term is chosen as tr(X^*)*2+2 and tr(Z^*)*2+2
+% Penal term is chosen as tr(X^*)*2 and tr(Z^*)*2
 
 %Authors: Feng-Yi Liao & Yang Zheng
 %         SOC Lab @UC San Diego
@@ -25,56 +25,63 @@ opts.epislon        = 10^-20;
 opts.beta           = 0.25; 
 opts.mu             = 0.5; 
 opts.alpha          = 50; %does not matter for adaptive case 
-opts.feasible       = false; 
-opts.adaptive       = true;
-opts.sparse         = false;
-opts.DynamicRho     = false; %useless but the currect version needs to keep it here
-opts.DynamicMaxCols = false; %useless but the currect version needs to keep it here
+
+% opts.feasible       = false; 
+% opts.adaptive       = true;
+% opts.sparse         = false;
+% opts.DynamicRho     = false;
+% opts.DynamicMaxCols = false;
 
 %%%%%%%%%% [Primal] %%%%%%%%%%
 %We do not count the first iteration for SBMP
 
-    opts.Maxiter     = 201;
-    opts.rho         = Optimal.TrZ*2+2;
-    opts.MaxCols     = 2;
-    opts.EvecPast    = 1;
-    opts.EvecCurrent = 1;
-    Out_Primal_1_1   = Copy_of_SBMP(At_sdp,b_sdp,c_sdp,K_sdp,opts);
- 
-    opts.Maxiter     = 201;
-    opts.rho         = Optimal.TrZ*2+2;
-    opts.MaxCols     = 3;
-    opts.EvecPast    = 2;
-    opts.EvecCurrent = 1;
-    Out_Primal_2_1   = Copy_of_SBMP(At_sdp,b_sdp,c_sdp,K_sdp,opts);
-
-    opts.Maxiter     = 201;
-    opts.rho         = Optimal.TrZ*2+2;
-    opts.MaxCols     = 4;
-    opts.EvecPast    = 3;
-    opts.EvecCurrent = 1;
-    Out_Primal_3_1   = Copy_of_SBMP(At_sdp,b_sdp,c_sdp,K_sdp,opts);
-
-    opts.Maxiter     = 201;
-    opts.rho         = Optimal.TrZ*2+2;
-    opts.MaxCols     = 2;
-    opts.EvecPast    = 0;
-    opts.EvecCurrent = 2;
-    Out_Primal_0_2   = Copy_of_SBMP(At_sdp,b_sdp,c_sdp,K_sdp,opts);
-  
-    opts.Maxiter     = 201;
-    opts.rho         = Optimal.TrZ*2+2;
-    opts.MaxCols     = 3;
-    opts.EvecPast    = 0;
-    opts.EvecCurrent = 3;
-    Out_Primal_0_3   = Copy_of_SBMP(At_sdp,b_sdp,c_sdp,K_sdp,opts);
-
-    opts.Maxiter     = 201;
-    opts.rho         = Optimal.TrZ*2+2;
-    opts.MaxCols     = 4;
-    opts.EvecPast    = 0;
-    opts.EvecCurrent = 4;
-    Out_Primal_0_4   = Copy_of_SBMP(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+%     opts.Maxiter     = 201;
+%     opts.rho         = Optimal.TrZ*2+2;
+%     opts.MaxCols     = 2;
+%     opts.EvecPast    = 1;
+%     opts.EvecCurrent = 1;
+%     opts.solver      = "primal";
+%     Out_Primal_1_1   = SBM(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+%  
+%     opts.Maxiter     = 201;
+%     opts.rho         = Optimal.TrZ*2+2;
+%     opts.MaxCols     = 3;
+%     opts.EvecPast    = 2;
+%     opts.EvecCurrent = 1;
+%     opts.solver      = "primal";
+%     Out_Primal_2_1   = SBM(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+% 
+%     opts.Maxiter     = 201;
+%     opts.rho         = Optimal.TrZ*2+2;
+%     opts.MaxCols     = 4;
+%     opts.EvecPast    = 3;
+%     opts.EvecCurrent = 1;
+%     opts.solver      = "primal";
+%     Out_Primal_3_1   = SBM(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+% 
+%     opts.Maxiter     = 201;
+%     opts.rho         = Optimal.TrZ*2+2;
+%     opts.MaxCols     = 2;
+%     opts.EvecPast    = 0;
+%     opts.EvecCurrent = 2;
+%     opts.solver      = "primal";
+%     Out_Primal_0_2   = SBM(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+%   
+%     opts.Maxiter     = 201;
+%     opts.rho         = Optimal.TrZ*2+2;
+%     opts.MaxCols     = 3;
+%     opts.EvecPast    = 0;
+%     opts.EvecCurrent = 3;
+%     opts.solver      = "primal";
+%     Out_Primal_0_3   = SBM(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+% 
+%     opts.Maxiter     = 201;
+%     opts.rho         = Optimal.TrZ*2+2;
+%     opts.MaxCols     = 4;
+%     opts.EvecPast    = 0;
+%     opts.EvecCurrent = 4;
+%     opts.solver      = "primal";
+%     Out_Primal_0_4   = SBM(At_sdp,b_sdp,c_sdp,K_sdp,opts);
 % 
 % 
 % %%%%%%%%%% [Dual] %%%%%%%%%%
@@ -85,21 +92,24 @@ opts.DynamicMaxCols = false; %useless but the currect version needs to keep it h
     opts.MaxCols     = 2;
     opts.EvecPast    = 0;
     opts.EvecCurrent = 2;
-    Out_Dual_0_2     = SBMD(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+    opts.solver      = "dual";
+    Out_Dual_0_2     = SBM(At_sdp,b_sdp,c_sdp,K_sdp,opts);
 %      
     opts.Maxiter     = 200;
     opts.rho         = Optimal.TrX*2+2;
     opts.MaxCols     = 3;
     opts.EvecPast    = 0;
     opts.EvecCurrent = 3;
-    Out_Dual_0_3     = SBMD(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+    opts.solver      = "dual";
+    Out_Dual_0_3     = SBM(At_sdp,b_sdp,c_sdp,K_sdp,opts);
     
     opts.Maxiter     = 200;
     opts.rho         = Optimal.TrX*2+2;
     opts.MaxCols     = 4;
     opts.EvecPast    = 0;
     opts.EvecCurrent = 4;
-    Out_Dual_0_4     = SBMD(At_sdp,b_sdp,c_sdp,K_sdp,opts);
+    opts.solver      = "dual";
+    Out_Dual_0_4     = SBM(At_sdp,b_sdp,c_sdp,K_sdp,opts);
 % 
 % save("results_rdSDPs_new\"+filename+"_result.mat",'Out_Primal_1_1','Out_Primal_2_1','Out_Primal_3_1','Out_Primal_0_2','Out_Primal_0_3','Out_Primal_0_4',...
 %      'Out_Dual_0_2','Out_Dual_0_3','Out_Dual_0_4');
