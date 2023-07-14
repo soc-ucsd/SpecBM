06f8f5ea387f712f3f488d7b4cad7d4784ad148a
FengYiLiao <robert147456@gmail.com>
2023-07-14 12:25:12 -0700

    Update SBMP.m

--- packages/SBM-Primal/SBMP.m
+++ packages/SBM-Primal/SBMP.m
@@ -64,34 +64,17 @@ function Out = SBMP(At,b,c,K,opts)
     DescentCount  = 1;
     DescentFlag   = true;
     
-%     if opts.DynamicRho
-%         ChangeRho     = false; %A variable that records if Rho has changed or not
-%     end
-%     if opts.DynamicMaxCols %If or not changing the number of selected eigenvectors after some threshold
-%         ChangeMaxCols = false;
-%     end
-    
 
     for iter = 1:Paras.Maxiter
         %Master problem
         if iter >1 
-           %if Paras.sparse %idle 
-               %[Wstar,X_next,Gammastar,Sstar,Dfeasi,gap] = Direction_QP_Primal_New(omegat_sdp,Paras,Wt,Pt,true);
-           %else
-               %[Wstar,X_next,Gammastar,Sstar,Dfeasi,gap,y] = Direction_QP_Primal(omegat,Paras,Wt,Pt,true);
-           %end
            [Wstar,X_next,Gammastar,Sstar,Dfeasi_psd,Dfeasi_free,gap,y,x_next,Dfeasi] = Direction_QP_Primal_Free(omegat_free,omegat_sdp,Paras,Wt,Pt,true);
         else
-           %if Paras.sparse  %idle 
-               %[Wstar,X_next,Gammastar,Sstar,Dfeasi,gap] = Direction_QP_Primal_New(omegat_sdp,Paras,Wt,Pt,false);
-           %else
-               %[Wstar,X_next,Gammastar,Sstar,Dfeasi,gap,y] = Direction_QP_Primal(omegat,Paras,Wt,Pt,false);
-           %end
            [Wstar,X_next,Gammastar,Sstar,Dfeasi_psd,Dfeasi_free,gap,y,x_next,Dfeasi] = Direction_QP_Primal_Free(omegat_free,omegat_sdp,Paras,Wt,Pt,false);
         end
         
         %We do not record the first iteration if the initial point is not feasible
-        if iter>1 %opts.feasible || iter>1
+        if iter>1
             DualFeasi(iter) = max(Dfeasi_psd,Dfeasi_free);
             Gap(iter) = gap;
         end
@@ -137,15 +120,7 @@ function Out = SBMP(At,b,c,K,opts)
             warning('something wrong');
             fprintf('f1-f2 = %.6f',EstimatedDrop);
             break;
-        end
-        %elseif RelativeAccuracy < Paras.epislon && iter >1 && PrimalFeasi(iter) < 10^-3 && DualFeasi(iter) < 10^-3
-%         if RelativeAccuracy < Paras.epislon && iter >1  && PrimalFeasi(iter) < 10^-3 && DualFeasi(iter) < 10^-3 && Complementary(iter) < 10^-3
-%             Obj(iter)           = f1;
-%             PrimalFeasi(iter)   = eig_val(I(1),I(1));
-%             RelativeAccry(iter) = RelativeAccuracy;
-%             break;
-%         end
-        
+        end        
         
         %Serious Step or Null Step        
         Y = reshape(-X_next,Paras.n,Paras.n);
@@ -158,16 +133,10 @@ function Out = SBMP(At,b,c,K,opts)
             eig_vec           = eig_vec(:,I);
             Vt                = eig_vec(:,1:Paras.EvecCurrent);
 
-            
-%             [Vt,eig_val] = eigs(Y,Paras.EvecCurrent,'la');
-%             f3           = Paras.c_free.'*x_next + Paras.c_sdp.'*X_next + Paras.rho*max([eigval(1);0]);
-
-
             %Update Pt
             if opts.EvecPast == 0
                 Pt = Vt;
             else
-                %[Pt,~]  = qr([Pt*Q1,Vt],0);
                 OldPt = Pt;
                 Pt = orth([Pt*Q1,Vt],1e-12);
             end 
@@ -208,27 +177,6 @@ function Out = SBMP(At,b,c,K,opts)
             DescentFlag = true;
         end
         
-%         %adjust Maxcols
-%         if (iter>1 && opts.DynamicMaxCols && ~ChangeMaxCols && (( RelativeAccuracy < opts.epislonphase1 && RelativePFeasi < opts.epislonphase1 && RelativeDFeasi < opts.epislonphase1 && RelativeGap <opts.epislonphase1 || iter >500 ) )) 
-%              Paras.MaxCols            = opts.MaxCols2;
-%              Paras.EvecPast           = opts.EvecPast2;
-%              Paras.EvecCurrent        = opts.EvecCurrent2;
-%              Paras.IndicesPSD         = Paras.IndicesPSD2;
-%              Paras.IndOffDiagPSD      = Paras.IndOffDiagPSD2;
-%              Paras.IndOffDiagCounter  = Paras.IndOffDiagCounter2;
-%              ChangeMaxCols            = true;
-%              Paras.Ir2                = eye(Paras.MaxCols^2);
-%              Paras.NumOfVar           = 1+Paras.MaxCols^2;
-%              fprintf('MaxCols change \n');
-%              Q1                = eig_vec(:,1:Paras.EvecPast);
-%              Q2                = eig_vec(:,Paras.EvecPast+1:end);
-%              Sigma2            = eig_val(Paras.EvecPast+1:end,Paras.EvecPast+1:end);
-%              if opts.EvecPast == 0
-%                 Vt      = eig_vec(:,1:Paras.EvecCurrent);
-%                 Pt      = Vt;
-%              end
-%         end
-        
         %Update Wt
         if opts.EvecPast> 0 
             if Gammastar< 0 %In case numerical issue
@@ -239,27 +187,18 @@ function Out = SBMP(At,b,c,K,opts)
         else
             Wt = (Wstar)/(trace(mat(Wstar)));
         end
-        
-        %improve numerical stability
-%         Wt([Paras.XIndOffDiag,Paras.XIndOffDiagCounter]) = ...
-%             1/2*(Wt([Paras.XIndOffDiag,Paras.XIndOffDiagCounter]) + Wt([Paras.XIndOffDiagCounter,Paras.XIndOffDiag]));
                 
-        
-        if iter>1%opts.feasible || 
+        if iter>1
             SemiFeasi           = min(-eig_val(I(1),I(1)),0);
             PrimalFeasi(iter)   = SemiFeasi;
             RelativeAccry(iter) = RelativeAccuracy;
 
         end
-%         eig_vec = eig_vec(:,I);
-%         Vt      = eig_vec(:,1:Paras.EvecCurrent);   
-%         [Pt,~]  = qr([Pt*Q1,Vt],0);
-        if iter>1 % opts.feasible || 
+
+        if iter>1
             Obj(iter)            = f1;
-            %Complementary(iter)  = abs(omegat_sdp.'*Wt);
         end
         
-
         if DescentFlag == true || iter == 0
             RelativePFeasi     = norm(Paras.At*omegat - Paras.b)/(1+normb);
             RelativePFeasi_Old = RelativePFeasi;
@@ -268,7 +207,6 @@ function Out = SBMP(At,b,c,K,opts)
         end
         
         if norm(Paras.c_free)~= 0
-            %RelativeDFeasi = max(sqrt(Dfeasi_psd)/(1+normC),sqrt(Dfeasi_free)/(1+normc_free));
             RelativeDFeasi  = sqrt(Dfeasi/(1+normC+normc_free));
         else
             RelativeDFeasi = sqrt(Dfeasi_psd)/(1+normC);
@@ -311,17 +249,7 @@ function Out = SBMP(At,b,c,K,opts)
         end
 
 
-%         %adjusting rho
-%         if iter>1 && Paras.DynamicRho && ~ChangeRho && PrimalFeasi(iter) > -10^-2
-%             Paras.rho = Paras.rho2; 
-%             fprintf('rho change to %f \n', Paras.rho2);
-%             ChangeRho = true;
-%         end
-
-
         if iter > 1 && mod(iter,OutOption.step) == 0                 
-%            fprintf('%5d  %7.2e  %7.2e  %9.2e  %9.2e  %8.2e  %8.2e  %8.2e %8.2e \n',...
-%            iter,f1,RelativeAccuracy,PrimalFeasi(iter),DualFeasi(iter), Gap(iter),Paras.alpha,toc(AlgorithmTime), Complementary(iter));
             fprintf('%5d  %7.2e  %7.2e  %9.2e  %9.2e  %9.2e  %10.2e  %7.1e  %7.2e \n',...
             iter,f1,RelativeAccuracy,RelativePFeasi,RelativeDFeasi, RelativeGap,SemiFeasi,Paras.alpha,toc(AlgorithmTime));
         end
@@ -336,22 +264,10 @@ function Out = SBMP(At,b,c,K,opts)
     Out.Iter           = iter;
     Out.PastEvec       = opts.EvecPast;
     Out.EvecCurrent    = opts.EvecCurrent;
-    %Out.Complementary  = Complementary;
-    %Out.DynamicRho     = opts.DynamicRho;
-
-%     RelativePFeasi     = norm(Paras.At*omegat - Paras.b)/(1+normb);
-%     RelativeDFeasi     = sqrt(Dfeasi)/(1+normC);
-%     RelativeGap        = gap/(1+abs(Paras.c.'*omegat)+abs(Paras.b.'*y));
-%     Out.RelativeDFeasi = RelativeDFeasi;
-%     Out.RelativePFeasi = RelativePFeasi;
-%     Out.RelativeGap    = RelativeGap;
-    
-%     Out.X              = omegat_sdp;
-%     Out.y              = y;
-%     Out.Z              = Wstar;
     Out.rho            = opts.rho;
     Out.cost           = Out.Obj(iter);
-    Out.DescentCount   = DescentCount-1;
+    Out.DescentCount   = DescentCount;
+
     % Print summary
     if Paras.verbose
         [~,myline1,~] = Header();
@@ -367,9 +283,7 @@ function Out = SBMP(At,b,c,K,opts)
         fprintf(' Semi residual        : %11.4e\n',Out.DescentPrimalSemiFeasi(end))
         fprintf(' Prepros time   (s)   : %11.4e\n',Out.PreprosTime);
         fprintf(' SBMP  time   (s)     : %11.4e\n',Out.MainAlgTime);
-        %fprintf(' Avg. conic proj (s)  : %11.4e\n',info.time.subiter(2)./iter)
         fprintf(' Avg. master prob (s) : %11.4e\n',Out.MainAlgTime./iter);
-        %fprintf(' Cleanup time (s)     : %11.4e\n',posttime)
         fprintf(' Total time   (s)     : %11.4e\n',Out.PreprosTime+Out.MainAlgTime);
         fprintf(myline1)
     end
@@ -416,8 +330,6 @@ function [Paras,OutOption] = Initialize(At,b,c,K,opts)
         Paras.Maxiter     = 500;% default
     end
     
-%     Paras.n           = opts.n;
-%     Paras.m           = opts.m;
     Paras.n           = K.s;
     Paras.m           = height(At);
 
@@ -456,12 +368,6 @@ function [Paras,OutOption] = Initialize(At,b,c,K,opts)
     else
         Paras.adaptive = true;%default
     end
-
-
-%     Paras.n_new            = 2*Paras.dx;
-%     Paras.NumOfVar_new     = Paras.n_new^2;
-%     Paras.NumOfVar_new_sym = Paras.n_new*(Paras.n_new+1)/2;
-%     Paras.NumOfP           = nchoosek(Paras.n/Paras.dx,2);%Number of Blocks
     
     [At,b,c,K,opts] = checkInputs(At,b,c,K,opts);
 
@@ -480,18 +386,6 @@ function [Paras,OutOption] = Initialize(At,b,c,K,opts)
     Paras.K                = K;
     Paras.MaxCols          = opts.MaxCols;
     Paras.NumOfVar         = 1+Paras.MaxCols^2;
-
-   
-%     %idle now
-%     Paras.DynamicRho       = opts.DynamicRho;
-%     if Paras.DynamicRho
-%         %rho start at rho1 and change rho2 when primal feasibility is low
-%         Paras.rho1        = opts.rho1;
-%         Paras.rho2        = opts.rho2;
-%         Paras.rho         = Paras.rho1;
-%     else%rho does not change
-%         Paras.rho         = opts.rho; 
-%     end
     
     Paras.rho         = opts.rho; 
 
@@ -509,17 +403,7 @@ function [Paras,OutOption] = Initialize(At,b,c,K,opts)
     [XIndSym,~,XIndOffDiag,~,~,XIndOffDiagCounter] = SymmetricIndices(Paras.n,false);
     Paras.XIndSym            = XIndSym;
     Paras.XIndOffDiag        = XIndOffDiag;
-    Paras.XIndOffDiagCounter = XIndOffDiagCounter;    
-    
-    
-
-%     if opts.DynamicMaxCols
-%         [xIndSym2,~,xIndOffDiag2,~,~,xIndOffDiagCounter2] = SymmetricIndices(opts.MaxCols2,false);
-%         Paras.IndicesPSD2        = xIndSym2;
-%         Paras.IndOffDiagPSD2     = xIndOffDiag2;
-%         Paras.IndOffDiagCounter2 = xIndOffDiagCounter2;
-%     end
-    
+    Paras.XIndOffDiagCounter = XIndOffDiagCounter;        
 
     %Output option
     OutOption.verbose     = 1;
@@ -536,7 +420,6 @@ function [Paras,OutOption] = Initialize(At,b,c,K,opts)
     if isfield(opts,'AAT_INV')
         Paras.AAT_INV = sparse(opts.AAT_INV); 
     else
-        %[Ats,~,~] = svecData(Paras.At_sdp,Paras.c_sdp,Paras.K_sdp);
         [Ats,~,~] = svecData(At,c,K);
         AAT       = Ats*Ats.';
         if isdiag(AAT)%only for MAXCUT
@@ -546,8 +429,6 @@ function [Paras,OutOption] = Initialize(At,b,c,K,opts)
         else
             L = chol(AAT);
             invL = inv(L);
-            %chol_AAT = chol(AAT);
-            %Paras.AAT_INV = inv(AAT);
             Paras.AAT_INV = invL*invL.';
             if issparse(Paras.AAT_INV)
                 Paras.AAT_INV = sparse(Paras.AAT_INV);
@@ -557,24 +438,6 @@ function [Paras,OutOption] = Initialize(At,b,c,K,opts)
 
     Paras.Ir2 = eye(Paras.MaxCols^2);
     Paras.q3  = -2*(Paras.At_sdp*Paras.c_sdp);
-    
-%     %Non-zero elements %not necessary now, because it does not speed up the
-%     %computation
-%     Paras.sparse = opts.sparse;
-%     if Paras.sparse
-%         [row,idx,val]             = find(Paras.At_sdp);
-%         Paras.At_sdp_nonzero.row  = row;
-%         Paras.At_sdp_nonzero.idx  = idx;
-%         Paras.At_sdp_nonzero.val  = val;
-% 
-%         [r,c]                     = ind2sub([Paras.n,Paras.n],idx);
-%         Paras.At_sdp_nonzero.Mrow = r;
-%         Paras.At_sdp_nonzero.Mcol = c;
-% 
-%         [~,idx]                 = find(Paras.c_sdp);
-%         %Paras.c_sdp_nonzero.row   = row;
-%         Paras.c_sdp_nonzero.idx   = idx;
-%     end
     
     Paras.PreprosTime = toc(proctime);
     PrintHeader(proctime,OutOption);
