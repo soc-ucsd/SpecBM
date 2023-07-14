aed7a1862975baa57d893e3eec8d609d5118419d
FengYiLiao <robert147456@gmail.com>
2023-07-14 13:48:25 -0700

    update

--- examples/POP/GenSpherePOP.m
+++ examples/POP/GenSpherePOP.m
@@ -7,8 +7,7 @@ for num = 1
         for z = 1:length(n)
             N = n(z);
             fprintf('num = %d,N = %d',num,N);
-            %mpvar('x',[N,1]);%Remeber to use "mpvar". It is faster than "syms"
-            x = sdpvar(N,1);
+            mpvar('x',[N,1]);%Remeber to use "mpvar". It is faster than "syms"
             function_name = "";
             f = 0;
             if function_number == 1 % "Broyden"
@@ -36,9 +35,6 @@ for num = 1
                 function_name = "Quartic";
             elseif function_number ==4 % "random" 
                 rng('default');
-%                 c = rand(nchoosek(n+4,n),1);
-%                 xx = monomials(x,[0,1,2,3,4]);
-%                 f = c.'*xx;
                 c = randn(nchoosek(n+4,n),1);
                 v = monolist(x,4);
                 f = c.'*v;
@@ -55,21 +51,7 @@ for num = 1
             h = h - R;
             degree = 5;
             option.name = function_name + '_sphere' + num2str(N) + '_R' + num2str(R);
-            gamma = sdpvar(1,1);
-            %[s1,c1] = polynomial(x,2);
-            phi_coe = sdpvar(nchoosek(n+2,n),1);
-            s       = monolist(x,2);
-            phi     = phi_coe.'*s;
-            F = sos(f-gamma-phi*h);
-            %solvesos(F,-gamma)
-            [model,recoverymodel] = export(F,-gamma,sdpsettings('solver','sedumi'),[gamma;phi_coe]); %export the problem data in sedumi form
-            At = model.A.';
-            b  = model.b;
-            c  = model.C;
-            K  = model.K;
-            save(option.name,'At','b','c','K');
-            %findbound_modify(f,[],h,degree,option);
-            %findbound(f,[],h,degree);
+            findbound_modify(f,[],h,degree,option);
         end
     end
 end
